d9428229671172c8ba840ac196fe064fce95f32f
shabbyjoon <ShabnamMokhtarani@Shabnams-MacBook-Pro.local>
2018-10-05 11:39:42 -0700

    generates csv and json data

--- database/seeding_scripts/CSVScripts/seedArtistToCSV.js
+++ database/seeding_scripts/CSVScripts/seedArtistToCSV.js
@@ -9,23 +9,41 @@ let writeStuff = (writeStream, artist, index) =>
     });
   });
 
-let artist;
+let artist = 'artistName,followed,verified,bio,images\n';
+
+let createImagesArray = () => {
+  let images = []
+  
+  function getRandomInt(max, min) {
+    return Math.floor(Math.random() * (max - min) + min);
+  }
+    for (let k = 1; k < 6; k++) {
+      let num = getRandomInt(501, 1)
+        images.push(`""https://s3.amazonaws.com/spotifyphotos/${num}.jpg""`)
+    }
+  
+    //console.log(images);
+    return images;
+  }
+
 const generateLoop = async () => {
   let start = 1;
   let stop = 200000;
-  artist = 'artistID,artistName,followed,verified\n';
   for (let i = 1; i < 51; i++) {
     const generateArtistData = function () {
       for (let j = start; j < stop + 1; j++) {
         artist +=
-          `${j},${faker.name.findName ()},${Math.round (Math.random ()) ? true : false}, ${Math.round(Math.random() * 0.7 + 0.3) ? true : false}` + '\n'
+        `${faker.name.findName ()},${Math.round (Math.random ()) ? true : false},${Math.round(Math.random() * 0.7 + 0.3) ? true : false},${faker.lorem.paragraphs(1)},"[${createImagesArray()}]"\n`
       }
     };
     generateArtistData ();
-    let writeStream = fs.createWriteStream ('./artistData2.csv', {"flags": "a"});
+    let writeStream = fs.createWriteStream (`./artistTableData.csv`, {"flags": "a"});
     await writeStuff (writeStream, artist, stop);
+    artist = '';
     start += 200000;
     stop += 200000;
   }
 };
 generateLoop ();
+
+
