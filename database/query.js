2d87b92c274eef66ad4d967766633039504df71d
shabbyjoon <ShabnamMokhtarani@Shabnams-MacBook-Pro.local>
2018-10-05 11:35:00 -0700

    updated query

--- database/query.js
+++ database/query.js
@@ -1,18 +1,6 @@
-// select 
-//   A.artistname, 
-//   A.verified, 
-//   A.followed ,
-//   A.bio,
-//   B.images,
-//   C.Locations,
-//   C.followersNumber
-// from artists as A
-// INNER JOIN images as B
-// on A.artistID = B.imageID 
-// INNER JOIN locations as C 
-// on A.artistID = C.locationID
-// where A.artistID= 5;
 
+//PSQL query
+/*
 select 
   A.artistname, 
   A.verified, 
@@ -25,4 +13,4 @@ from artists as A
 INNER JOIN locations as C 
 on A.artistID = C.locationID
 where A.artistID= 5;
-
+*/
