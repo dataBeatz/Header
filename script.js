adee1048a9d501c9952cfa0cda2f054a8e556e66
shabbyjoon <ShabnamMokhtarani@Shabnams-MacBook-Pro.local>
2018-10-15 10:52:40 -0700

    updates k6 test for optimization

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ import { check, sleep } from "k6";
 
 export let options = {
   vus: 300,
-  rps: 2000,
-  duration: "120s"
+  rps: 500,
+  iterations: 10000
 };
 
 export default function() {
   let id = Math.floor(Math.random() * 999999)
-  let res = http.get("http://localhost:3004/artists/" + id);
+  let res = http.get("http://54.173.223.33/artists/" + id);
   //sleep(1);
   check(res, {
     "status200": (r) => r.status == 200,
